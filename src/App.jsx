46a2af75608aa242e6f77466227ad02d8fe9ff6c
Bilsyp <bilalsyah77@gmail.com>
2024-07-22 12:43:24 +0700

    change padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { data } from "./data/dessert";
 import Cart from "./components/Cart";
 const App = () => {
   return (
-    <div className=" container  my-10 p-1    mx-auto">
+    <div className=" container  my-10 Ds:p-10    mx-auto">
       <div className="  Ds:grid Ds:grid-cols-6 Ds:gap-5">
         <div className=" Ds:col-span-4">
           <h1 className="text-[#2c140e]  text-4xl pb-4 font-bold Ds:text-6xl md:text-center Ds:text-left">
